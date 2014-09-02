f75bddc9d7bfef14a41e26090ec04f638484fe01
jx2ob <joo216@lehigh.edu>
2014-09-02 03:38:59 +0000

    updated WelcomClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+//////////////////////////////////////////////////////////////////////////////
+//JJ O'Brien
+//Hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+    //  add main method
+    public static void main(String[] args) {
+        
+        //print the statement 
+        System.out.println("   ----------- ");
+        System.out.println("   | WELCOME | ");
+        System.out.println("   ----------- ");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-J--O--O--2--1--6-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("This summer I worked at Ben Franklin Technology Partners as a Summer Associate.");
+        
+    }
+}
