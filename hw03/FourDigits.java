d766a062b3ca4271fb82694ee01fead00377e65f
jx2ob <joo216@lehigh.edu>
2014-09-17 04:16:37 +0000

    updated FourDigits

--- hw03/FourDigits.java
+++ hw03/FourDigits.java
@@ -6,12 +6,27 @@
 //      javac FourDigits.java
 //  run the program
 //      java FourDigits
+//  Program tells user to enter a double then prints the first four digits to the right of the decimal point
 
+//  set up scanner
+import java.util.Scanner;
 //  define a class
-
 public class FourDigits {
     //main method required for every Java program
     public static void main(String[] args) {
+        Scanner myScanner;
+        myScanner = new Scanner(System.in);
+        
+        System.out.print("Enter a double and I display the four digits to the right of the decimal point- ");
+        double myNumber=myScanner.nextDouble();
+        
+        int Cast=(int)(myNumber); //turn number into an integer
+        double result;  //make result a variable
+        result=myNumber-Cast;   //calculate the result
+        result*=10000;
+        int castCast=(int)(result); //turns the number into an integer
+        
+        System.out.printf("The four digits are "+castCast+"%n");
         
     }
 }
