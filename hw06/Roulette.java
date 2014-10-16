ecf03ccd0da33a2b1a269ab703e54900bc6c488b
jx2ob <joo216@lehigh.edu>
2014-10-16 23:36:10 +0000

    updated hw06

--- hw06/Roulette.java
+++ hw06/Roulette.java
@@ -2,20 +2,17 @@
 //10/14/14
 //hw06 Roulette java program
 //
-//Program 
+//Program is a monte Carlo simulation that checks the probability of winning roulette in 1000 different 100 spin situations
 //
 //  First compile the program
 //      javac Roulette.java
 //  run the program
 //      java Roulette
 
-//Create Scanner
-//import java.util.Scanner;
 
 public class Roulette {
     public static void main(String args[]) {
-        //Scanner s = new Scanner(System.in);
-        final int TRIALS = 1000;
+        final int TRIALS = 1000; //set up variables
         int myNum = 1;
         int prize = 36;
         int trial = 0;
@@ -24,29 +21,29 @@ public class Roulette {
         int totalWinnings = 0;
         int profitSessions = 0;
         int wins = 0;
-        while (trial < 1000) {
+        while (trial < 1000) { // set up loop
             int round = 0;
             int numOfMatches = 0;
-            while (round < 100) {
+            while (round < 100) { //set up second loop
                 int spin =  (int) (1 + (Math.random() *  38));
-                if (spin == myNum){
+                if (spin == myNum){ // records wins
                     wins++;
                     numOfMatches++;
                 }
-                if (round == 99){
+                if (round == 99){ // records losses
                     if (numOfMatches == 0){
                         numLosses++;
                     }
-                    else if (numOfMatches >= 3){
+                    else if (numOfMatches >= 3){ //records times with profit
                         profitSessions++;
                     }
                 }    
-               round++;
+               round++;// continues loop
             }
-            trial++; 
+            trial++; // continues loop
         }
         
-        System.out.println("The number of times you lose everything is: " + numLosses);
+        System.out.println("The number of times you lose everything is: " + numLosses);// prints out answers
         System.out.println("Your total winnings are: $" + (wins * prize));
         System.out.println("The number of times you walked away with a profit is: " + profitSessions);
     }
